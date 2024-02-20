e3867e8dedf2ce87cd792df9510e3bdb0b7e6043
maqsad-muradovich <maqsad.baxadirov@gmail.com>
2024-02-20 19:21:51 +0500

    WIP

--- handlers/users/sendusersfile.py
+++ handlers/users/sendusersfile.py
@@ -1,23 +1,23 @@
-from loader import dp, db, bot
-from data.config import ADMINS
+# from loader import dp, db, bot
+# from data.config import ADMINS
 
-from aiogram import types
+# from aiogram import types
 
-@dp.message_handler(commands='sendusersfile')
-async def send_file(message: types.Message):
-    users = db.select_all_users()
-    users = [item for item in users if len(db.get_followers_list(item[0]))>2]
-    users.sort()
+# @dp.message_handler(commands='sendusersfile')
+# async def send_file(message: types.Message):
+#     users = db.select_all_users()
+#     users = [item for item in users if len(db.get_followers_list(item[0]))>2]
+#     users.sort()
 
-    header = "User ID,Name\n"
+#     header = "User ID,Name\n"
 
-    data = ""
-    for user_id, name in users:
-        data += f"{user_id},{name}\n"
+#     data = ""
+#     for user_id, name in users:
+#         data += f"{user_id},{name}\n"
 
-    with open('data/users.csv', 'w') as file:
-        file.write(data)
-        file.write(header)
+#     with open('data/users.csv', 'w') as file:
+#         file.write(data)
+#         file.write(header)
 
-    with open('data/users.csv', 'rb') as file:
-        await bot.send_document(file)
+#     with open('data/users.csv', 'rb') as file:
+#         await bot.send_document(file)
