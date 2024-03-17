4e954a39d0a855b5f83ab446f047fdc67eda755f
maqsad-muradovich <maqsad.baxadirov@gmail.com>
2024-03-17 18:53:14 +0500

    Finished

--- handlers/users/myusers.py
+++ handlers/users/myusers.py
@@ -1,7 +1,13 @@
 from aiogram import types
-from loader import dp, db
+from loader import dp, db, bot
+from data.config import CHANEL_ID
 
 @dp.message_handler(commands='myusers')
 async def my_users(message: types.Message):
-    text = db.get_followers_list(user_id=message.from_user.id)
-    await message.answer(f"Siz chaqirgan akkauntlar soni: {len(text)}")
+    usersList = db.get_followers_list(user_id=message.from_user.id)
+    countUsers = int()
+    for userId in usersList:
+        member = await bot.get_chat_member(chat_id=CHANEL_ID, user_id=userId)
+        if member.status in ['member', 'administrator', 'creator']:
+            countUsers += 1
+    await message.answer(f"Siz chaqirgan akkauntlar soni: {countUsers}")
